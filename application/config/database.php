e8575b7f371fc9e887e0832436e5d88359e27a3e
gnguyen <gia01020304@gmail.com>
2019-07-04 10:36:21 +0700

    f5

--- application/config/database.php
+++ application/config/database.php
@@ -76,8 +76,8 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'uwm',
-	'password' => '123456',
+	'username' => 'root',
+	'password' => '',
 	'database' => 'mathmanipulative',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
@@ -94,4 +94,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-$db['default']["port"]=3307;
+$db['default']["port"]=3306;
