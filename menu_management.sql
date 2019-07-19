cde29df1e9908bd3422ba48bce892be28371d335
Son Cao Vo <sonvc@primas.net>
2019-07-19 16:15:24 +0700

    King

--- menu_management.sql
+++ menu_management.sql
@@ -27,9 +27,25 @@ CREATE TABLE `menu_management` (
   `text` varchar(255) NOT NULL,
   `visible` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
 
 /*Data for the table `menu_management` */
+
+insert  into `menu_management`(`id`,`key`,`value`,`text`,`visible`) values 
+(29,'background','background','Backgrounds',''),
+(30,'attributeBlock','attributeBlock','Attribute Blocks',''),
+(31,'algebraTiles','algebraTiles','Algebra Tiles',''),
+(32,'baseTenBlocks','baseTenBlocks','Base Ten Blocks',''),
+(33,'centimeterCubes','centimeterCubes','Centimeter Cubes',''),
+(34,'connectingcubes','connectingcubes','Connecting Cubes',''),
+(35,'clock','clock','Clock',''),
+(36,'integerCounters','integerCounters','Integer Counters',''),
+(37,'fractionTiles','fractionTiles','Fraction Tiles',''),
+(38,'fractioncircle','fractioncircle','Fraction Circles',''),
+(39,'money','money','Money',''),
+(40,'numberLine','numberLine','Number Line',''),
+(41,'thermometer','thermometer','Thermometer',''),
+(42,'patternBlocks','patternBlocks','Pattern Blocks','');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
