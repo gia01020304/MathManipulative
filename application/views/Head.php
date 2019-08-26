498c6cf4c8d6e098672ee8b37a7ffd5250d3685a
gnguyen <gia01020304@gmail.com>
2019-08-26 16:35:12 +0700

    Change background

--- application/views/Head.php
+++ application/views/Head.php
@@ -1,4 +1,4 @@
-<?php $version = '1.3.6' ?>
+<?php $version = '1.3.7' ?>
 <title>Math Manipulation</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
