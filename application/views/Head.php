b5da2c36ee0284123e4a465c5f398fb0394f65c0
gnguyen <gia01020304@gmail.com>
2019-09-16 11:37:20 +0700

    Fix Free hand

--- application/views/Head.php
+++ application/views/Head.php
@@ -1,4 +1,4 @@
-<?php $version = '1.4.1' ?>
+<?php $version = '1.4.2' ?>
 <title>Math Manipulation</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
